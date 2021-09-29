313eee9d75aaf84dc255de3a12ddfd39890f81c4
murrey.xiao <122392319@qq.com>
2021-09-29 17:16:27 +0800

    优化奖品列表

--- src/views/tree/index.vue
+++ src/views/tree/index.vue
@@ -3,11 +3,18 @@
     <el-input v-model="filterText" placeholder="任意字符搜索" style="margin-bottom:30px;" />
 
     <el-tree
-      ref="tree2"
-      :data="data2"
+      id="treeId"
+      ref="prizeTree"
+      class="filter-tree"
+      show-checkbox
+      draggable
+      node-key="id"
+      :data="data"
       :props="defaultProps"
       :filter-node-method="filterNode"
-      class="filter-tree"
+      :default-expanded-keys="[21, 22, 23, 24]"
+      :default-checked-keys="[7, 16, 17,]"
+      @check="(click, checked)=>{handleCheckChange(click, checked)}"
     />
 
   </div>
@@ -19,82 +26,97 @@ export default {
   data() {
     return {
       filterText: '',
-      data2: [
+      data: [
         {
-          label: '杭银消金',
+          id: 21,
+          label: 'Small Prize( In ￥50 )',
           children: [{
-            label: '产品',
-            children: [
-              {
-                label: '周东颖 (dongyin.zhou)'
-              }, {
-                label: '杨志彬 (beck.yang)'
-              }
-            ]
+            id: 1,
+            label: '美食 · 杨枝甘露'
           },
           {
-            label: '开发',
-            children: [{
-              label: '王文豪 (wenhao.wang)'
-            }]
+            id: 2,
+            label: '美食 · 三层牛肉汉堡'
           },
           {
-            label: '测试',
-            children: [{
-              label: '肖沐磊 (lette.xiao)'
-            }]
+            id: 3,
+            label: '美食 · 麦麦脆汁鸡'
           }]
         },
         {
-          label: '甘肃银行',
+          id: 22,
+          label: 'Medical Prize( ￥50 ~ ￥150 )',
           children: [{
-            label: '产品',
-            children: [
-              {
-                label: '白定超 (steve.bai)'
-              },
-              {
-                label: '杨倩 (zoey.yang)'
-              }
-            ]
+            id: 4,
+            label: '日用 · 木质书夹'
           },
           {
-            label: '开发',
-            children: [{
-              label: '马锐 (rui.ma)'
-            }]
+            id: 6,
+            label: '游戏 · Switch 存储卡512G'
           },
           {
-            label: '测试',
-            children: [{
-              label: '肖沐磊 (lette.xiao)'
-            }]
+            id: 7,
+            label: '日用 · 小米台灯'
           }]
         },
         {
-          label: '兰州银行',
+          id: 23,
+          label: 'Big Prize( ￥150 ~ ￥500 )',
           children: [{
-            label: '产品',
-            children: [
-              {
-                label: '王嘉铭 (chloe.wang)'
-              },
-              {
-                label: '龙建宇 (beck.yang)'
-              }
-            ]
+            id: 8,
+            label: '电脑配件 · 内存条1'
           },
           {
-            label: '开发',
-            children: [{
-              label: '何凌宇 (lingyu.he)'
-            }]
+            id: 9,
+            label: '电脑配件 · 内存条2'
           },
           {
-            label: '测试',
-            children: [{
-              label: '肖沐磊 (lette.xiao)'
-            }]
+            id: 10,
+            label: '电脑配件 · 固态256G'
+          },
+          {
+            id: 11,
+            label: '电脑配件 · 机械1T'
+          },
+          {
+            id: 12,
+            label: '电脑配件 · 电源'
+          },
+          {
+            id: 13,
+            label: '数码 · 小型航拍无人机'
+          },
+          {
+            id: 14,
+            label: '其他 · 情侣手表'
+          },
+          {
+            id: 15,
+            label: '会员 · 微信读书年度会员'
+          },
+          {
+            id: 16,
+            label: '会员 · B 站年度会员'
+          },
+          {
+            id: 17,
+            label: '游戏 · Switch 马里奥奥德赛'
+          }]
+        },
+        {
+          id: 24,
+          label: 'Large Prize( Up ￥500 )',
+          children: [{
+            id: 18,
+            label: '电脑配件 · 显卡1650s'
+          },
+          {
+            id: 19,
+            label: '电脑配件 · 主板+CPU'
+          },
+          {
+            id: 20,
+            label: '数码 · 读书笔记打印机'
           }]
         }],
       defaultProps: {
@@ -105,7 +127,7 @@ export default {
   },
   watch: {
     filterText(val) {
-      this.$refs.tree2.filter(val)
+      this.$refs.prizeTree.filter(val)
     }
   },
 
@@ -113,6 +135,12 @@ export default {
     filterNode(value, data) {
       if (!value) return true
       return data.label.indexOf(value) !== -1
+    },
+    handleCheckChange(click, checked) {
+      console.log(checked.checkedKeys)
+      console.log('全选', checked.checkedNodes)
+      console.log(checked.halfCheckedKeys)
+      console.log('半选', checked.halfCheckedNodes)
     }
   }
 }
